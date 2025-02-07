e1fb9607c67370c6a859e5f48a006f809e437e75
someoneorderedatransfem <someoneorderedatransfem@gmail.com>
2025-02-07 23:15:39 +1000

    Made Card Animations Cleaner

--- Scripts/card_test.gd
+++ Scripts/card_test.gd
@@ -1,50 +1,37 @@
 extends Control
 
-@onready var original_position: Vector2 = position - Vector2(0, 20)
-@onready var drag_origin: Vector2 = Vector2(0, 0)
-@onready var animationvar: float = 0
+var drag_origin_angle: float
+var drag_origin_magnitude: float
+@onready var origin_position: Vector2 = position
+@onready var target_position: Vector2 = position
 @onready var target_angle: float = 0
-@onready var target_position: Vector2 = original_position
+@onready var animationvar: float = 0
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("ready")
-	connect('mouse_entered', on_mouse_enter)
-	connect('mouse_exited', on_mouse_exit)
-	connect('gui_input', on_mouse_click)
+	connect("gui_input", on_gui_input)
 
-func on_mouse_enter() -> void:
-	position.y -= 20
-
-func on_mouse_exit() -> void:
-	position.y += 20
-
-func on_mouse_click(event: InputEvent) -> void:
+func on_gui_input(event: InputEvent):
 	if event.is_class("InputEventMouseButton"):
 		event = event as InputEventMouseButton
 		if event.pressed:
 			animationvar = 0
-			drag_origin = get_local_mouse_position()
+			drag_origin_angle = Vector2().angle_to_point(get_local_mouse_position())
+			drag_origin_magnitude = Vector2().distance_to(get_local_mouse_position())
 		else:
-			animationvar = 0
-			drag_origin = Vector2(0,0)
+			target_position = origin_position
 			target_angle = 0
-			target_position = original_position
-		print(event)
 	elif event.is_class("InputEventMouseMotion"):
+		animationvar = 0.5
 		event = event as InputEventMouseMotion
-		if event.pressure == 1.00:
-			target_position = get_global_mouse_position()
-			target_angle = (position-drag_origin).angle_to(get_global_mouse_position())
-			print(target_angle)
+		if event.pressure == 1:
+			target_position = get_global_mouse_position()-Vector2(drag_origin_magnitude*cos(drag_origin_angle+rotation), drag_origin_magnitude*sin(drag_origin_angle+rotation))
+			target_angle = (target_position).angle_to(position)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if animationvar < 1:
-		if target_position != original_position:
-			animationvar += delta*0.2
-		else:
-			animationvar += delta*0.6
-		position = position.lerp(target_position-drag_origin, animationvar)
+		animationvar += delta*0.4
+		print(animationvar)
+	else:
+		animationvar = 1
+	position = position.lerp(target_position, animationvar)
 	rotation = lerp(rotation, target_angle, animationvar)
-	pass
