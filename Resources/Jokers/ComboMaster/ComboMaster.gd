5536ee490aa94dbc927feb6fa7fb85c10922a3fa
someoneorderedatransfem <someoneorderedatransfem@gmail.com>
2025-02-17 16:25:21 +1000

    awawrwawr

--- Resources/Jokers/ComboMaster/ComboMaster.gd
+++ Resources/Jokers/ComboMaster/ComboMaster.gd
@@ -3,4 +3,4 @@ extends JokerAffect
 func affect(cards: Array[Card], score: Dictionary, _jokers: Array[Joker]):
 	# Add 0.5 multiplier for each card beyond the first
 	if cards.size() > 1:
-		score.mult += (cards.size() - 1) * 0.5 
+		score.mult *= (cards.size() - 1) * 0.5 
