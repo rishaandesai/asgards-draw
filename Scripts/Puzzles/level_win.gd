a88227e17e46ac469d7e5f9e448d4490cdc7ceda
someoneorderedatransfem <someoneorderedatransfem@gmail.com>
2025-04-02 15:52:46 +1000

    labyrinth

--- Scripts/Puzzles/level_win.gd
+++ Scripts/Puzzles/level_win.gd
@@ -3,7 +3,7 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.name == "Player":
-		load_overworld()
+		dep_load_next_level()
 
 func load_overworld() -> void:
 	get_tree().change_scene_to_file.call_deferred("res://Scenes/World.tscn")
@@ -11,7 +11,7 @@ func load_overworld() -> void:
 
 func dep_load_next_level() -> void:
 	var currentScene: StringName = owner.scene_file_path
-	var nextArray: PackedStringArray = DirAccess.get_files_at("res://Scenes/Puzzle/Puzzles")
+	var nextArray: PackedStringArray = DirAccess.get_files_at("res://Scenes/Puzzle/Dungeons")
 	if nextArray.has(currentScene.get_file()):
 		nextArray.remove_at(nextArray.find(currentScene.get_file()))
-	get_tree().change_scene_to_file.call_deferred("res://Scenes/Puzzle/Puzzles/"+nextArray[RandomNumberGenerator.new().randi_range(0, nextArray.size()-1)])
+	get_tree().change_scene_to_file.call_deferred("res://Scenes/Puzzle/Dungeons/"+nextArray[RandomNumberGenerator.new().randi_range(0, nextArray.size()-1)])
